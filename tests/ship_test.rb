948d289bf5097826e032133523c0c83945edd2c7
Kevin Krato <kratok08@gmail.com>
2019-10-08 15:43:12 -0600

    Write pseudocode for ShipTest Co-authored-by: LainMcGrath <lain.mcgrath@gusto.com>

---
+++ tests/ship_test.rb
@@ -0,0 +1,21 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative "../lib/ship.rb";
+
+class ShipTest < Minitest::Test
+  # it should exist
+
+  # newly instantiated objects should accept a name and length argument
+
+  # it should return a value when name when object.name is invoked
+
+  # it should return a value when length when object.length is invoked
+
+  # it should return a value when health when object.health is invoked
+
+  # it should return boolean when object.sunk? method is invoked
+
+  # it should return a value when hit when object.hit is invoked
+
+end
