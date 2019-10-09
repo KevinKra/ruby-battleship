1526d6f7c55cf0c77a80298a9a86884dcf0bbe0a
Kevin Krato <kratok08@gmail.com>
2019-10-08 18:12:09 -0600

    Add additional behaviors to Cell class
    Co-authored-by: LainMcGrath <lain.mcgrath@gusto.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,11 @@
 class Cell
-  attr_reader :coordinate, :ship, :empty
+  attr_reader :coordinate, :ship, :empty, :fired_upon, :render
   def initialize(coordinate, ship = nil)
     @coordinate = coordinate.upcase
     @ship = ship
     @empty = true
+    @fired_upon = false
+    @render = "."
   end
   
   def empty?
@@ -16,6 +18,33 @@ class Cell
     else
       @ship = ship
       @empty = false
+    end
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @fired_upon == true
+      return "You've already targeted this coord, wut u doin"
+    end 
+    @fired_upon = true
+    if empty == true
+      @render = "M"
+      "you missed lul"
+    else
+      @render = "H"
+      @ship.hit
+    end
+  end
+
+  # Resolve the render issue, prop vs method
+  def render(bool)
+    if bool == true
+      if @empty == false
+        "S"
+      end
     end
   end
 end
