c576617c5644a81754f0cddb88277ab1a83d980d
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-15 12:22:57 -0600

    Add edited tests to accomidate edited cell hash

--- tests/board_test.rb
+++ tests/board_test.rb
@@ -18,11 +18,13 @@ class BoardTest< Minitest::Test
 
   #test it has the correct number of cells
   def test_it_has_the_correct_number_of_cells
-    assert_equal 16, @board.cells.length
+    assert_equal 16, @board.generate_game.length
   end
 
   #test it validates coordinates
   def test_the_coordinates_are_valid
+    @board.generate_game
+
     assert_equal true, @board.valid_coordinate?("A1")
     assert_equal false, @board.valid_coordinate?("ZZ")
   end
@@ -30,8 +32,7 @@ class BoardTest< Minitest::Test
   #test if placement is valid
   def test_the_placement_is_valid
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
-    @board.cells["A2"].empty = false
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "B1", "C1"])
   end
 
   #test the output of split arrays
@@ -58,11 +59,22 @@ class BoardTest< Minitest::Test
     assert_equal false, @board.verify_placement_format([["A", "A", "A"], ["1","2","4"]])
   end
 
-  def test_it_places_ship_on_board 
-    @board.place(@cruiser, ["A1", "A2", "A3"]) 
+  def test_it_places_ship_on_board
+
+  @board.generate_game
+  
+     cell_1 = @board.cells["A1"]
+     cell_2 = @board.cells["A2"]
+     cell_3 = @board.cells["A3"]
+
+    @board.place(@cruiser, ["A1", "A2", "A3"])
     assert_equal false, @board.cells["A1"].empty?
     assert_equal false, @board.cells["A2"].empty?
     assert_equal false, @board.cells["A3"].empty?
     assert_equal true, @board.cells["A4"].empty?
   end
+  #
+  # def test_render
+  #
+  # end
 end
