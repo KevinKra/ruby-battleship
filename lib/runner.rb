1030ec822e3825d3ee3ba868c62d0c8193ecf312
Kevin Krato <kratok08@gmail.com>
2019-10-15 11:07:35 -0600

    Update runner to reach start_game

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,13 +1,18 @@
 require "./board.rb"
 require "./cell.rb"
 require "./ship.rb"
+require "./play_game.rb"
 require 'pry'
 
 board = Board.new()
 submarine = Ship.new("Submarine", 2)
+game = Play.new
 
 cruiser = Ship.new("Cruiser", 3)
-# board.place(cruiser, ["A1", "A2", "A3"])
+board.place(cruiser, ["A1", "A2", "A3"])
+board.place(submarine, ["C1", "C2"])
+
+game.start_game
 
 
 
