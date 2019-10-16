2c4cdb0107acc0a612b97210415d3091eff78e11
Kevin Krato <kratok08@gmail.com>
2019-10-15 18:49:25 -0600

    Update board placement methods to iterate over array of ships

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -4,34 +4,25 @@ class ComputerPlayer
   end
 
   def place_ships_on_board(total_ships, board)
-    placement = generate_placement(total_ships, board)
-    p placement
-    board.place(total_ships[1], placement)
+    total_ships.each do |ship|
+      placement = generate_placement(ship, board)
+      board.place(ship, placement)
+    end
   end
 
-  def generate_placement(ships_to_place, board)
+  def generate_placement(ship, board)
+    # This (yet) does not deal with oversaturation edge cases.
     # determine currently available cells
     open_cells = board.cells.select {|key, value| value.empty == true}.map() { |key, value| key}
-    ship_size = ships_to_place[1].length
-    if open_cells.length != 0
-      starting_letter = open_cells.sample.split('')[0]
-    else
-      puts "There are no more available cells."
-    end
+    starting_letter = open_cells.sample.split('')[0] 
 
-    possible_placement = open_cells.select {|coord| coord.split('').include?(starting_letter)}
-
-    until possible_placement.length >= ship_size do 
+    possible_placement = []
+    until possible_placement.length >= ship.length do 
       possible_placement = open_cells.select {|coord| coord.split('').include?(starting_letter)}
     end
 
-    if possible_placement.length == ship_size
-      return possible_placement
-    else possible_placement.length > ship_size
-      return possible_placement[0..ship_size - 1]
-    end
-
+    possible_placement.length == ship.length ? 
+      possible_placement : 
+      possible_placement[0..ship.length - 1]
   end
-
-
 end
