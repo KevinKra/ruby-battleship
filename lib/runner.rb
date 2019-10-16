616f2c3a1d7e80f119c6db5da188cd433823810c
Kevin Krato <kratok08@gmail.com>
2019-10-15 20:27:53 -0600

    Update runner

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,19 +1,22 @@
 require "./board.rb"
 require "./cell.rb"
 require "./ship.rb"
-require "./play.rb"
+require "./game.rb"
+require_relative "./computer_player.rb"
 require 'pry'
 
-board = Board.new()
+computer_board = Board.new
+player_board = Board.new
 submarine = Ship.new("Submarine", 2)
-game = Play.new
-
 cruiser = Ship.new("Cruiser", 3)
-board.place(cruiser, ["A1", "A2", "A3"])
-board.place(submarine, ["C1", "C2"])
+cruiser2 = Ship.new("Cruiser", 3)
+computer_player = ComputerPlayer.new
+game = Game.new
 
-game.start_game
+# board.place(cruiser, ["A1", "A2", "A3"])
+# board.place(submarine, ["C1", "C2"])
+# binding.pry
+# computer_player.place_ships_on_board([cruiser, submarine], computer_board)
+game.init_game
 
 
-
-binding.pry
