cd65dfe2e9de010e910e446f5658293cc86b0c0f
Kevin Krato <kratok08@gmail.com>
2019-10-15 20:27:37 -0600

    Allow player to place ships

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -2,4 +2,25 @@ class HumanPlayer
   def initialize
     
   end
+
+  def start_game(total_ships, board)
+    notify_start_game
+    place_ships(total_ships, board)
+  end
+
+  def notify_start_game
+    puts "\n You now need to lay out your two ships." + "\n The Cruiser is two units long and the Submarine is three units long. \n"
+  end
+
+  def place_ships(ships, board)
+    puts board.render(true)
+    ships.each do |ship|
+      puts "Enter the squares for the #{ship.name} (#{ship.length} places):"
+      input = gets.chomp.upcase().split(" ")
+      # !still need to validate
+      p input
+      board.place(ship, input)
+      puts board.render(true)
+    end
+  end
 end
